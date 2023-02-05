0e96d7551f81959bafa065d3d65860c3c6b3ee87
Adesh <adeshp948@gmail.com>
2023-02-05 03:14:41 -0500

    Adding problem 2 solution on Arrays and function

--- Server_side_JavaScript_in_Node/Arrays and Objects/Problem/Problem.md
+++ Server_side_JavaScript_in_Node/Arrays and Objects/Problem/Problem.md
@@ -1,7 +1,18 @@
 # Perform Array Operations
 
-### Problem Statement
+### Problem Statement 1
 
 For a given array of product prices write the javascript function to get their prices post deduction of given discount
 
 Discount is provided in percentage
+
+
+### Problem Statement 2
+
+For the given array of product with prices, write the JavaScript function to Filter out the premium products.
+
+Products with price above 300, are marked as Premium Products.
+
+Apply a 15% discount on all the non-premium products.
+
+Create a stock of each product type.
--- Server_side_JavaScript_in_Node/Arrays and Objects/Problem/script.js
+++ Server_side_JavaScript_in_Node/Arrays and Objects/Problem/script.js
@@ -1,11 +1,55 @@
+// # Problem 1 Solution
 
-const products = [["Gucci Belt",300],["Smiley T-shirt",50],["Shinie Nail Paint",10]];
+// const products = [["Gucci Belt",300],["Smiley T-shirt",50],["Shinie Nail Paint",10]];
 
-const calculateDiscountedPrice=(discount)=>{
-    for(let i=0;i<products.length;i++) products[i][1]=products[i][1]-(products[i][1]*discount)/100;
-}
+// const calculateDiscountedPrice=(discount)=>{
+//     for(let i=0;i<products.length;i++) products[i][1]=products[i][1]-(products[i][1]*discount)/100;
+// }
+
+// calculateDiscountedPrice(10);
+
+// By using map function
+
+// const calculateDiscountedPrice = discount => products.map(
+//     product=> [product[0],product[1]-product[1]*discount/100]
+// )
+// let discountedPrice = calculateDiscountedPrice(10)
+
+// console.log(products);
+// console.log(discountedPrice)
+
+// # Problem 2 Solution
+
+const products = [
+  ["Gucci Round Bucklet Belt", 400],
+  ["Gucci Round Bucklet Belt", 450],
+  ["Esbeda Wallet", 250],
+  ["Gucci Round Bucklet Belt", 300],
+  ["Smiley T-Shirt", 500],
+  ["Smiley T-Shirt", 50],
+  ["Smiley T-Shirt", 250],
+  ["Shinie Nail Paint", 100],
+];
+
+ const premiumProducts = products.filter(product => product[1]>300);
+
+ console.log(premiumProducts)
 
 
-calculateDiscountedPrice(10);
+ const filteredProducts = products.filter(product=>product[1]<=300);
 
-console.log(products);
+ const calculateDiscountedPrice = filteredProducts.map(
+    product=>[product[0],product[1]-product[1]*0.15]
+ )
+
+let productStock = products.reduce((stocks,product)=>{
+    let stockItem = stocks.find(stock=> stock[0]==product[0]);
+    if(!stockItem) stocks.push([product[0],1])
+    else ++stockItem[1];
+    return stocks;
+},[])
+
+console.log(productStock);
+
+ console.log(filteredProducts)
+ console.log(calculateDiscountedPrice)
