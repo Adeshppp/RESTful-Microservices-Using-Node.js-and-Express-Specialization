17acc118fb36e5703488214be9f6b76cf2da289f
Adesh <adeshp948@gmail.com>
2023-02-23 11:08:25 -0500

    Adding few other synchronous functions to practice

--- Server_side_JavaScript_in_Node/Week 5/File demo/app.js
+++ Server_side_JavaScript_in_Node/Week 5/File demo/app.js
@@ -1,7 +1,7 @@
 const fs= require('fs');
 
 const loadNotes = ()=>{
-    const buffer = fs.readFileSync('./notes.json');
+    const buffer = fs.readFileSync('notes.json');
     const dataStr = buffer.toString();
     const obj = JSON.parse(dataStr);
     return obj;
@@ -25,4 +25,26 @@ const saveNotes= (notes) => {
     fs.writeFileSync('notes.json', dataStr);
 }
 
-module.exports = {addNotes}
+const getAllNotes = () =>{
+    const notes = loadNotes();
+    notes.forEach(note => console.log(note));
+}
+
+const readNotes = (title) =>{
+    const notes = loadNotes();
+    var note = notes.find(note => note.title===title)
+    if(note){
+        console.log("title : ",title," author : ",note.author);
+    }
+    else console.log("Note not found!!!")
+}
+
+const removeNote=(title)=>{
+    const notes = loadNotes();
+    const index = notes.findIndex(note=> note.title===title)
+    if(index!=-1) notes.splice(index,1);
+    else console.log("Note not found!!!")
+    saveNotes(notes);
+}
+
+module.exports = {addNotes,readNotes,getAllNotes,removeNote}
--- Server_side_JavaScript_in_Node/Week 5/File demo/index.js
+++ Server_side_JavaScript_in_Node/Week 5/File demo/index.js
@@ -2,3 +2,13 @@ const data = require("./app");
 
 
 data.addNotes('title1','author1')
+
+data.getAllNotes();
+
+data.readNotes('title1')
+
+data.removeNote('title1')
+
+data.getAllNotes();
+
+data.readNotes('The Grapes of Wrath')
